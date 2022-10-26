8f236c8ee2c94fe2c052007b50a5d8cd10c65cb7
zml <747765207@qq.com>
2022-10-26 23:17:34 +0800

    mdf '更改配置信息'

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,71 @@
-import { fileURLToPath, URL } from 'node:url'
+import {
+  fileURLToPath,
+  URL
+} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {
+  defineConfig,
+  loadEnv
+} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({
+  command,
+  mode
+}) => {
+  // 根据当前工作目录中的 `mode` 加载 .env 文件
+  // 设置第三个参数为 '' 来加载所有环境变量，而不管是否有 `VITE_` 前缀。
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [vue(), vueJsx(),
+      // 打包压缩，主要是本地gzip，如果服务器配置压缩也可以
+      viteCompression(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src',
+          import.meta.url))
+      }
+    },
+    define: {
+      'process.env': env
+    },
+    terserOptions: {
+      compress: {
+        // warnings: false,
+        // drop_console: true,  //打包时删除console
+        drop_debugger: true, //打包时删除 debugger
+        // pure_funcs: ['console.log'],
+      },
+      output: {
+        // 去掉注释内容
+        comments: true,
+      },
+    },
+    server: {
+      open: '/index.html',
+      port: 9886,
+      proxy: {
+        '/json': {
+          target: `http://192.168.1.121`,
+          changeOrigin: true
+        },
+        '/temp': {
+          target: `http://192.168.1.121`,
+          changeOrigin: true
+        },
+        '/qsdi': {
+          target: `http://192.168.1.121:8888`,
+          changeOrigin: true
+        },
+        '/ivdg': {
+          target: `http://192.168.1.121:8888`,
+          changeOrigin: true
+        }
+      }
     }
   }
 })
